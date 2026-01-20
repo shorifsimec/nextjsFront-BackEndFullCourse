ddadfa46bbe7f939ef415b3b8fa9ec79ea652070
Shorif <shorifsimec@gmail.com>
2026-01-20 13:17:18 +0600

    updated

--- src/app/about/[id]/page.tsx
+++ src/app/about/[id]/page.tsx
@@ -3,7 +3,8 @@ const AboutDetails = async ({ params }: { params: Promise<{ id: string }> }) => 
 
   const { id } = await params
   return (
-    <div>AboutDetails
+    <div>
+      <p>AboutDetails</p>
       <p>onek gulo data dekhate ba onek gulo data niye kaj korar jonno async await use korte hoy</p>
       {id}</div>
   )
